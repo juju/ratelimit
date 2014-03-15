199b73b09237ea8d566bdd9a028f428bfee0de4b
Roger Peppe <rogpeppe@gmail.com>
2014-03-15 17:10:12 +0000

    implement Reader and Writer

--- reader.go
+++ reader.go
@@ -1,65 +1,47 @@
-// +build ignore
+package ratelimit
 
-type Reader struct {
-	r io.Reader
-	flow byteFlow
+import "io"
+
+type reader struct {
+	r      io.Reader
+	bucket *TokenBucket
 }
 
-
-// NewReader returns a reader that is rate limited by
+// Reader returns a reader that is rate limited by
 // the given token bucket. Each token in the bucket
-// represents one byte. Note that means this reader
-// cannot be used for rate limiting of more than
-// 1GB/s, as that would require a fill interval of
-// less than 1 ns. Also, very high flow rates will
-// be inaccurate
-func NewReader(r io.Reader, bucket *TokenBucket, quantum int) io.Reader {
-	
+// represents one byte.
+func Reader(r io.Reader, bucket *TokenBucket) io.Reader {
+	return &reader{
+		r:      r,
+		bucket: bucket,
+	}
 }
 
-func (r *Reader) Read(buf []byte) (int, error) {
-	n, err := r.Read(buf)
+func (r *reader) Read(buf []byte) (int, error) {
+	n, err := r.r.Read(buf)
 	if n <= 0 {
 		return n, err
 	}
-	r.flow.wait(n)
+	r.bucket.Wait(int64(n))
 	return n, err
 }
 
-type byteFlow struct {
-	bucket *tokenBucket
-	borrowed int
-	quantum int
+type writer struct {
+	w      io.Writer
+	bucket *TokenBucket
 }
 
-func (f *byteFlow) wait(n int) {
-	if n < r.borrowed {
-		r.borrowed -= n
-		return
+// Writer returns a reader that is rate limited by
+// the given token bucket. Each token in the bucket
+// represents one byte.
+func Writer(w io.Writer, bucket *TokenBucket) io.Writer {
+	return &writer{
+		w:      w,
+		bucket: bucket,
 	}
-	// Pay back any bytes we borrowed earlier.
-	n -= r.borrowed
-	// Calculate number of tokens we need, rounding
-	// up if necessary.
-	tokens := (n + r.quantum - 1) / r.quantum
-	r.bucket.Wait(tokens)
-
-	// If we took more than we strictly need,
-	// pay it back later.
-	r.borrowed = tokens * r.quantum - len(buf)
 }
 
-type Writer struct {
-	w io.Writer
-	flow byteFlow
-}
-
-
-func NewWriter(w io.Writer, bucket *TokenBucket, quantum int) io.Reader {
-	
-}
-
-func (w *Writer) Write(buf []byte) (int, error) {
-	w.flow.wait(len(buf))
+func (w *writer) Write(buf []byte) (int, error) {
+	w.bucket.Wait(int64(len(buf)))
 	return w.w.Write(buf)
 }
